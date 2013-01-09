8fe33398be58adc82f307c94eaa3084d1b86a5d8
Ashok Vijayendra <ashok-vijayendra@uiowa.edu>
2013-01-09 15:44:39 -0600

    adding attachment

--- lib/surveyor/parser.rb
+++ lib/surveyor/parser.rb
@@ -274,7 +274,7 @@ class Answer < ActiveRecord::Base
       self.text_args("Other").merge({:response_class => "string"})
     when :none, :omit # is_exclusive erases and disables other checkboxes and input elements
       self.text_args(arg.to_s.humanize).merge({:is_exclusive => true})
-    when :integer, :float, :date, :time, :datetime, :text, :datetime, :string
+    when :integer, :float, :date, :time, :datetime, :text, :datetime, :string,:attachment
       self.text_args(arg.to_s.humanize).merge({:response_class => arg.to_s, :display_type => "hidden_label"})
     end
   end
